e2fcc174ef75a49f5aabcbb3e24d0f8070f948bf
Ashan Lakshitha <143149942+AshanLakshitha16763@users.noreply.github.com>
2024-08-29 00:09:35 +0530

    chore: Update page titles and fix typos

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -5,7 +5,7 @@ export default function ContactMe() {
         <p className="sub--title">Get In Touch</p>
         <h2>Contact Me</h2>
         <p className="text-lg">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
+          You can contact me using the below form.I will get back to you as soon.
         </p>
       </div>
       <form className="contact--form--container">

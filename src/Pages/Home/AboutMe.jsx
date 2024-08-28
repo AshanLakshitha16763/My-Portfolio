ee70a9b278074d09a013b185809a3ea58249f0a3
Ashan Lakshitha <143149942+AshanLakshitha16763@users.noreply.github.com>
2024-08-29 00:09:54 +0530

    click button change

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,17 +8,14 @@ export default function AboutMe() {
         <div className="hero--section--content">
           <p className="section--title"></p>
           <h1 className="skills-section--heading">About Me</h1>
+          
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
+          Hello! I’m Ashan Lakshitha, an enthusiastic undergraduate student at Sabaragamuwa University, currently pursuing a degree in Computing and Information Systems. I am passionate about full-stack development and have chosen to specialize in the MERN stack (MongoDB, Express.js, React, Node.js).
           </p>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae?
+          I am a quick learner and a problem solver who thrives in collaborative environments. I am currently seeking an internship in full-stack development to further enhance my skills and contribute to innovative projects. I am eager to apply my knowledge and work closely with others to achieve common goals and drive success.
           </p>
+
         </div>
       </div>
     </section>
